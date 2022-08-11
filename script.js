d1944cfa6c0204895134d402fe29a1831e5d184f
singh-kritya <kritya2024@gmail.com>
2022-08-11 18:11:26 +0530

    Prerequisites completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const count=0;
     function httpreq(){
 
-        var c=document.getElementById("d2");
+        var c=document.getElementById("d3");
 
 var xhr = new XMLHttpRequest();
 
@@ -19,7 +19,10 @@ xhr.onreadystatechange = function(){
                                var node=document.createElement("img");
 
                                  node.src=obj[i].url;
-
+                                 const mj=JSON.stringify(node);
+if(i==0){
+    c.innerHTML=mj;
+}
                                  c.appendChild(node);
     
 
@@ -32,4 +35,41 @@ xhr.onreadystatechange = function(){
            xhr.open("GET","https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09");
 
            xhr.send();
+}
+
+
+
+
+function cat(str){
+
+    var c=document.getElementById("d2");
+
+var xhr = new XMLHttpRequest();
+
+console.log(xhr);
+
+xhr.onreadystatechange = function(){
+
+       if(this.readyState == 4 && this.status == 200){
+
+                  var data=this.responseText;
+
+
+                c.innerHTML=data;
+                        //    var node=document.createElement("div");
+
+                        //      node.innerHTML=data;
+
+                        //      c.appendChild(node);
+
+
+                
+
+                 
+
+       }}
+
+       xhr.open("GET",str);
+
+       xhr.send();
 }
